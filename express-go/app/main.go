98b4adbebea1aff7fd148bd8fd6e6d2cbc1a4ebc
Maymoona Al Balushi <maymoona.al.boloshi@gmail.com>
2026-05-29 20:54:39 +0400

    Handle connections concurrently

--- express-go/app/main.go
+++ express-go/app/main.go
@@ -29,48 +29,52 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
+		go handleConnection(connection)
 
-		buf := make([]byte, 1024)
-		n, err := connection.Read(buf)
-
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-		}
-
-		req := string(buf[:n])
-		lines := strings.Split(req, CRLF)
-
-		path := strings.Split(lines[0], " ")[1]
-
-		pathParts := strings.Split(path, "/")
-		fmt.Println(path)
-
-		var res string
-		if path == "/" {
-			res = "HTTP/1.1 200 OK\r\n\r\n"
-
-		} else if pathParts[1] == "echo" {
-			echoStr := pathParts[2]
-			res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoStr), echoStr)
-
-		} else if pathParts[1] == "user-agent" {
-			for line := 1; line < len(lines); line++ {
-				keyVal := strings.Split(lines[line], ":")
-				if keyVal[0] == "User-Agent" {
-					userAgent := strings.TrimSpace(keyVal[1])
-					res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
-				}
-				fmt.Println(keyVal)
-			}
-
-		} else {
-			res = "HTTP/1.1 404 Not Found\r\n\r\n"
-
-		}
-		fmt.Println(res)
-
-		connection.Write([]byte(res))
-
-		connection.Close()
 	}
+}
+
+func handleConnection(connection net.Conn) {
+	buf := make([]byte, 1024)
+	n, err := connection.Read(buf)
+
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+	}
+
+	req := string(buf[:n])
+	lines := strings.Split(req, CRLF)
+
+	path := strings.Split(lines[0], " ")[1]
+
+	pathParts := strings.Split(path, "/")
+	fmt.Println(path)
+
+	var res string
+	if path == "/" {
+		res = "HTTP/1.1 200 OK\r\n\r\n"
+
+	} else if pathParts[1] == "echo" {
+		echoStr := pathParts[2]
+		res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoStr), echoStr)
+
+	} else if pathParts[1] == "user-agent" {
+		for line := 1; line < len(lines); line++ {
+			keyVal := strings.Split(lines[line], ":")
+			if keyVal[0] == "User-Agent" {
+				userAgent := strings.TrimSpace(keyVal[1])
+				res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
+			}
+			fmt.Println(keyVal)
+		}
+
+	} else {
+		res = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+	}
+	fmt.Println(res)
+
+	connection.Write([]byte(res))
+
+	connection.Close()
 }
