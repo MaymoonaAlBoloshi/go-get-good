bcea4f97f8f2a222b0196d3919017df7bf3406c8
Maymoona Al Balushi <maymoona.al.boloshi@gmail.com>
2026-05-29 20:54:38 +0400

    Parse request path and handle missing routes

--- express-go/app/main.go
+++ express-go/app/main.go
@@ -4,28 +4,51 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
+const CRLF = "\r\n"
+
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	app, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
+	connection, err := app.Accept()
 
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	buf := make([]byte, 1024)
+	_, err = connection.Read(buf)
+
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+	}
+
+	req := string(buf)
+	lines := strings.Split(req, CRLF)
+	path := strings.Split(lines[0], " ")[1]
+	fmt.Println(path)
+
+	var res string
+	if path == "/" {
+		res = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		res = "HTTP/1.1 404 OK\r\n\r\n"
+	}
+	fmt.Println(res)
+
+	connection.Write([]byte(res))
+
 }
